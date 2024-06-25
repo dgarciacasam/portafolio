e14859a218de7a8a4b2d1e8cf86024b84ac3b920
saddxni <dgarciacasam@gmail.com>
2024-06-26 00:22:33 +0200

    Separados datos del componente experience

---
+++ src/data/experience.ts
@@ -0,0 +1,8 @@
+export const EXPERIENCE = [
+    {
+        date: "2023 - 2024",
+        role: "Desarrollador fullstack",
+        title: "NTT Lapso",
+        description: "Desarrollando una aplicación para acelerar los procesos internos de la empresa automatizando la carga de datos en diferentes aplicaciones como OneRP o herramientas de Microsoft 365 mediante hojas de cálculo.",
+    }
+]
