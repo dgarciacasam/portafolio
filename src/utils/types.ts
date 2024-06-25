f97fe66417759deb1bb6c256b38decc7125bb8c1
saddxni <dgarciacasam@gmail.com>
2024-06-26 00:25:06 +0200

    Creado archivo de tipos y añadida vista de posts dinámica

---
+++ src/utils/types.ts
@@ -0,0 +1,22 @@
+export type PostCardProps = {
+    title: string
+    description: string
+    link:string
+    tag: string
+    date: string
+}
+
+export type ProjectCardProps = {
+    title: string
+        description: string
+        link: string
+        githubLink: string
+        stack: string[]
+        icon: string
+}
+
+export type ExperienceCardProps = {
+    date: string
+    title: string
+    description: string
+}
