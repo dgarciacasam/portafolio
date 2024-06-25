ed1f93e0927a413b3cdea15740e351a9a5042554
saddxni <dgarciacasam@gmail.com>
2024-06-26 00:22:48 +0200

    Separados datos del componente projects

---
+++ src/data/projects.ts
@@ -0,0 +1,27 @@
+export const PROJECTS = [
+    {
+        title: "Rocket",
+        description: "Una aplicación para la gestión de proyectos y tareas.",
+        link: "https://approcket.vercel.app/",
+        githubLink: "https://github.com/dgarciacasam/rocket",
+        stack: ["React", "Typescript", "Tailwind"],
+        icon: "RocketIcon"
+
+        
+    },{
+        title: "RocketAPI",
+        description: "API REST con implementación de tareas Cron y JWT.",
+        link: "",
+        githubLink: "https://github.com/dgarciacasam/rocketAPI",
+        stack: ["Java (Spring Boot)", "JWT", "MariaDB" ],
+        icon: "RocketIcon"
+    },
+    {
+        title: "Portafolio",
+        description: "Código de mi portafolio web.",
+        link: "",
+        githubLink: "https://github.com/dgarciacasam/portafolio",
+        stack: ["Astro", "Typescript"],
+        icon: "PlanetIcon"
+    }
+]
