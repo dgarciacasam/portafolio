bb82c8babae02c8846a5fa89534daca975622f0b
dgarciacasam <dgarciacasam@gmail.com>
2024-08-03 10:02:39 +0200

    Creado nuevo proyecto cursor maker

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,27 +1,35 @@
 export const PROJECTS = [
-    {
-        title: "Rocket",
-        description: "Una aplicación para la gestión de proyectos y tareas.",
-        link: "https://approcket.vercel.app/",
-        githubLink: "https://github.com/dgarciacasam/rocket",
-        stack: ["React", "Typescript", "Tailwind"],
-        icon: "RocketIcon"
-
-        
-    },{
-        title: "RocketAPI",
-        description: "API REST con implementación de tareas Cron y JWT.",
-        link: "",
-        githubLink: "https://github.com/dgarciacasam/rocketAPI",
-        stack: ["Java (Spring Boot)", "JWT", "MariaDB" ],
-        icon: "RocketIcon"
-    },
-    {
-        title: "Portafolio",
-        description: "Código de mi portafolio web.",
-        link: "",
-        githubLink: "https://github.com/dgarciacasam/portafolio",
-        stack: ["Astro", "Typescript"],
-        icon: "PlanetIcon"
-    }
+  {
+    title: 'Rocket',
+    description: 'Una aplicación para la gestión de proyectos y tareas.',
+    link: 'https://approcket.vercel.app/',
+    githubLink: 'https://github.com/dgarciacasam/rocket',
+    stack: ['React', 'Typescript', 'Tailwind'],
+    icon: 'RocketIcon',
+  },
+  {
+    title: 'RocketAPI',
+    description: 'API REST con implementación de tareas Cron y JWT.',
+    link: '',
+    githubLink: 'https://github.com/dgarciacasam/rocketAPI',
+    stack: ['Java (Spring Boot)', 'JWT', 'MariaDB'],
+    icon: 'RocketIcon',
+  },
+  {
+    title: 'Portafolio',
+    description: 'Código de mi portafolio web.',
+    link: '',
+    githubLink: 'https://github.com/dgarciacasam/portafolio',
+    stack: ['Astro', 'Typescript'],
+    icon: 'PlanetIcon',
+  },
+  {
+    title: 'Cursor Maker',
+    description:
+      'Genera paquetes de cursores en los colores que quieras utilizando el cursor de MacOS como base',
+    link: 'https://cursormaker.vercel.app',
+    githubLink: 'https://github.com/dgarciacasam/cursor-maker',
+    stack: ['React', 'JavaScript', 'Tailwind'],
+    icon: 'LinkIcon',
+  },
 ]
